dd88d4362fbff701c27db285406538a34b7062d0
chenmingang <1026610078@qq.com>
2015-11-30 21:48:44 +0800

    Update try.sql

--- doc/try.sql
+++ doc/try.sql
@@ -11,25 +11,26 @@ create table user(
 );
 
 create table trytryagain(
-id int(11),,
+id int(11) unsigned NOT NULL AUTO_INCREMENT,
 tags varchar(128),
 userId int(11),
 info varchar(1024),
 img varchar(128),
+content text,
 createTime datetime,
 updateTime datetime,
 isDel int(1) default 0,
 primary  key (id)
 );
 
-create table step(
-id int(11),
-tryId int(11),
-stepName varchar (256),
-stepNo int(11),
-content text,
-createTime datetime,
-updateTime datetime,
-isDel int(1) default 0,
-primary key (id)
-);
+-- create table step(
+-- id int(11),
+-- tryId int(11),
+-- stepName varchar (256),
+-- stepNo int(11),
+-- content text,
+-- createTime datetime,
+-- updateTime datetime,
+-- isDel int(1) default 0,
+-- primary key (id)
+-- );
