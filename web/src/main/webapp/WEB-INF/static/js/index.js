cacbff9cfeabda782130eabd1fbd2fb156306298
momocmg <1026610078@qq.com>
2015-12-07 22:38:52 +0800

    init

--- web/src/main/webapp/WEB-INF/static/js/index.js
+++ web/src/main/webapp/WEB-INF/static/js/index.js
@@ -22,4 +22,22 @@ MyApp.controller('indexCtr', function ($scope, $http, $timeout) {
             }
         });
     }
+    $scope.detail = function(id) {
+        var url = "/get?id="+id;
+        $http.get(url).success(function (data) {
+            if (data) {
+                $scope.try = data;
+            }
+        });
+    }
+});
+$("#trys").on('click','h4',function(){
+    //var tryitemdiv = $($(this).parent()).parent();
+    //var trysdiv = $(tryitemdiv).parent();
+    //var width = trysdiv[0].clientWidth;
+    //tryitemdiv.animate({width:width+'px'},"slow");
+    ////tryitemdiv.removeClass('try-item');
+    //$('#trys').masonry({
+    //    itemSelector: '.try-item'
+    //});
 });
