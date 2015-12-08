c78362e28e31c22a74d23a27b723f94e377c4095
momocmg <1026610078@qq.com>
2015-12-08 22:35:06 +0800

    init

--- web/src/main/webapp/WEB-INF/static/js/index.js
+++ web/src/main/webapp/WEB-INF/static/js/index.js
@@ -27,17 +27,8 @@ MyApp.controller('indexCtr', function ($scope, $http, $timeout) {
         $http.get(url).success(function (data) {
             if (data) {
                 $scope.try = data;
+                $("#modal").click();
             }
         });
     }
-});
-$("#trys").on('click','h4',function(){
-    //var tryitemdiv = $($(this).parent()).parent();
-    //var trysdiv = $(tryitemdiv).parent();
-    //var width = trysdiv[0].clientWidth;
-    //tryitemdiv.animate({width:width+'px'},"slow");
-    ////tryitemdiv.removeClass('try-item');
-    //$('#trys').masonry({
-    //    itemSelector: '.try-item'
-    //});
 });
